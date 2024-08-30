b48d9ec0bc25361324dd6a386a61deb4e002cd34
acongkece <stokkcstore@gmail.com>
2024-08-31 06:52:29 +0700

    .nikparse.sh

--- .nikparse.sh
+++ .nikparse.sh
@@ -5,6 +5,8 @@ clear
     npm i nik-parse -g
     read -p "MASUKIN TARGET:" nik
     nik-parse -n $nik
-    sleep3
-    clear
+    read -p "UNTUK KEMBALI PRESS ENTER:" b
+    cd $home
+    cd XcngForget-v3.5
     bash .menu.sh
+    
